831723f0ff940b826484c08224cd4ae95eebdc23
d-zhukov <d.zhukov@samsung.com>
2019-06-10 17:29:47 +0300

    VTSU-522. 1. Fixed missing asserts

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import sys
 
 DATASET_PATH = "/dbstore/datasets/"
 PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(PROJECT_PATH, 'submodules/struct2depth'))
+sys.path.append(os.path.join(PROJECT_PATH, 'submodules/tf_models/research/struct2depth'))
 sys.path.append(os.path.join(PROJECT_PATH, 'submodules', 'tfoptflow/tfoptflow'))
