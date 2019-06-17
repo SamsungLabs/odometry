86c1f6172d2465f5d35cb7665f4e67b709b3b560
a.vorontsova <a.vorontsova@samsung.com>
2019-06-17 15:20:16 +0300

    Refactored Parser

--- odometry/preprocessing/data_parser.py
+++ odometry/preprocessing/data_parser.py
@@ -9,44 +9,54 @@ import math
 import pyquaternion
 from functools import partial
 
+from odometry.linalg.linalg_utils import split_se3
+
 
 class BaseParser:
-    def __init__(self,
-                 sequence_directory):
-        self.sequence_directory = sequence_directory
+
+    def __init__(self, trajectory_dir):
+        self.dir = trajectory_dir
         self.cols = ['path_to_rgb', 'path_to_depth']
-    
+
     def _load_data(self):
-        pass
-
-
-    def _calculate_global_pose_matrices(self):
-        pass
+        raise NotImplementedError
 
     def _make_absolute_filepath(self):
         for col in self.cols:
-            self.global_dataframe[col] = self.global_dataframe[col].apply(
-                lambda filename: os.path.join(self.directory, filename))
-    
-    def _create_global_dataframe(self):
-        pass
+            self.df[col] = self.df[col].apply(
+                lambda filename: os.path.abspath(os.path.join(self.src_dir, filename)))
+
+    def _create_dataframe(self):
+        raise NotImplementedError
 
     def run(self):
         self._load_data()
-        self._create_global_dataframe()
+        self._create_dataframe()
         self._make_absolute_filepath()
-        return self.global_dataframe
+        return self.df
+
+
+class ElementwiseParser(BaseParser):
+
+    def _parse_item(self, item):
+        raise NotImplementedError
+
+    def _create_dataframe(self):
+        trajectory_parsed = [self._parse_item(item) for item in self.trajectory]
+        self.df = pd.DataFrame.from_dict(trajectory_parsed)
 
 
 class TUMParser(BaseParser):
+
     def __init__(self,
-                 sequence_directory,
-                 directory):
-        super(TUMParser, self).__init__(sequence_directory)
-        self.directory = directory
-        self.gt_txt_path = os.path.join(self.directory, 'groundtruth.txt')
-        self.depth_txt_path = os.path.join(self.directory, 'depth.txt')
-        self.rgb_txt_path = os.path.join(self.directory, 'rgb.txt')
+                 trajectory_dir,
+                 src_dir):
+        super(TUMParser, self).__init__(trajectory_dir)
+        self.src_dir = src_dir
+        self.gt_txt_path = os.path.join(self.src_dir, 'groundtruth.txt')
+        self.depth_txt_path = os.path.join(self.src_dir, 'depth.txt')
+        self.rgb_txt_path = os.path.join(self.src_dir, 'rgb.txt')
+        self.skiprows = 3
 
     @staticmethod
     def associate_timestamps(timestamps, other_timestamps, max_difference=0.02):
@@ -57,7 +67,7 @@ class TUMParser(BaseParser):
                              for other_timestamp in other_timestamps
                              if abs(timestamp - other_timestamp) < max_difference]
         potential_matches.sort(key=lambda x: abs(x[0] - x[1]))
-        
+
         matches = []
         for timestamp, other_timestamp in potential_matches:
             if timestamp in timestamps and other_timestamp in other_timestamps:
@@ -67,91 +77,78 @@ class TUMParser(BaseParser):
 
         matches.sort()
         return list(zip(*matches))
-    
+
     @staticmethod
     def associate_dataframes(dataframes, timestamp_cols):
-        dataframe = dataframes[0]
+        df = dataframes[0]
         timestamp_col = timestamp_cols[0]
-        for other_dataframe, other_timestamp_col in zip(dataframes[1:], timestamp_cols[1:]):
+        for other_df, other_timestamp_col in zip(dataframes[1:], timestamp_cols[1:]):
             timestamps, other_timestamps = \
-                TUMParser.associate_timestamps(dataframe[timestamp_col].values, other_dataframe[other_timestamp_col].values)
-            dataframe = dataframe[dataframe[timestamp_col].isin(timestamps)]
-            dataframe.index = np.arange(len(dataframe))
-            other_dataframe = other_dataframe[other_dataframe[other_timestamp_col].isin(other_timestamps)]
-            other_dataframe.index = timestamps
-            
-            assert len(dataframe) == len(other_dataframe)
-            dataframe = dataframe.join(other_dataframe, on=timestamp_col)
-        return dataframe
+                TUMParser.associate_timestamps(df[timestamp_col].values, other_df[other_timestamp_col].values)
+            df = df[df[timestamp_col].isin(timestamps)]
+            df.index = np.arange(len(df))
+            other_df = other_df[other_df[other_timestamp_col].isin(other_timestamps)]
+            other_df.index = timestamps
+
+            assert len(df) == len(other_df)
+            df = df.join(other_df, on=timestamp_col)
+        return df
 
     def _load_txt(self, txt_path, columns):
-        dataframe = pd.read_csv(txt_path, skiprows=3, sep=' ', index_col=False, names=columns)
-        dataframe.columns = columns
+        df = pd.read_csv(txt_path, skiprows=self.skiprows, sep=' ', index_col=False, names=columns)
+        df.columns = columns
         timestamp_col = columns[0]
-        dataframe[timestamp_col] = dataframe[timestamp_col].apply(float)
-        return dataframe
+        df[timestamp_col] = df[timestamp_col].apply(float)
+        return df
+
+    def _load_gt_txt(self):
+        return self._load_txt(self.gt_txt_path, columns=['timestamp_gt', 't_x', 't_y', 't_z', 'q_x', 'q_y', 'q_z', 'q_w'])
+
+    def _load_rgb_txt(self):
+        return self._load_txt(self.rgb_txt_path, columns=['timestamp_rgb', 'path_to_rgb'])
+
+    def _load_depth_txt(self):
+        return self._load_txt(self.depth_txt_path, columns=['timestamp_depth', 'path_to_depth'])
 
     def _load_data(self):
-        gt_dataframe = self._load_txt(self.gt_txt_path, columns=['timestamp_gt', 't_x', 't_y', 't_z', 'q_x', 'q_y', 'q_z', 'qw'])
-        depth_dataframe = self._load_txt(self.depth_txt_path, columns=['timestamp_depth', 'path_to_depth'])
-        rgb_dataframe = self._load_txt(self.rgb_txt_path, columns=['timestamp_rgb', 'path_to_rgb'])
-        self.dataframes = [depth_dataframe, rgb_dataframe, gt_dataframe]
+        self.dataframes = [self._load_depth_txt(), self._load_rgb_txt(), self._load_gt_txt()]
         self.timestamp_cols = ['timestamp_depth', 'timestamp_rgb', 'timestamp_gt']
 
-    def _create_global_dataframe(self):
-        self.global_dataframe = self.associate_dataframes(self.dataframes, self.timestamp_cols)
+    def _create_dataframe(self):
+        self.df = self.associate_dataframes(self.dataframes, self.timestamp_cols)
 
     def __repr__(self):
-        return 'TUMParser(dir={}, txt_path={})'.format(self.sequence_directory, self.gt_txt_path)
-    
+        return 'TUMParser(dir={}, txt_path={})'.format(self.dir, self.gt_txt_path)
+
+
 class RetailBotParser(TUMParser):
     
     def __init__(self,
-                 sequence_directory,
-                 directory):
-        super(RetailBotParser, self).__init__(sequence_directory, directory)
-        self.gt_txt_path = os.path.join(self.directory, 'pose.txt')
-        self.depth_txt_path = os.path.join(self.directory, 'depth.txt')
-        self.rgb_txt_path = os.path.join(self.directory, 'rgb.txt')
-    
-    def _load_txt(self, txt_path, columns):
-        dataframe = pd.read_csv(txt_path, skiprows=0, sep=' ', index_col=False, names=columns)
-        dataframe.columns = columns
-        timestamp_col = columns[0]
-        dataframe[timestamp_col] = dataframe[timestamp_col].apply(float)
-        return dataframe
-    
-    def _load_pic(self, txt_path, columns):
-        dataframe = pd.read_csv(txt_path, skiprows=0, sep=' ', index_col=False, names=columns)
-        dataframe.columns = columns
-        timestamp_col = columns[0]
-        pic = columns[1]
-        dataframe[timestamp_col] = dataframe[timestamp_col].apply(float)
-        dataframe[pic] = dataframe[pic].apply(lambda x : self.directory + x[1:])
-        return dataframe
-    
-    def _load_data(self):
-        gt_dataframe = self._load_txt(self.gt_txt_path, columns=['timestamp_gt', 't_x', 't_y', 't_z', 'q_x', 'q_y', 'q_z', 'q_w'])
-        depth_dataframe = self._load_pic(self.depth_txt_path, columns=['timestamp_depth', 'path_to_depth'])
-        rgb_dataframe = self._load_pic(self.rgb_txt_path, columns=['timestamp_rgb', 'path_to_rgb'])
-        self.dataframes = [depth_dataframe, rgb_dataframe, gt_dataframe]
-        self.timestamp_cols = ['timestamp_depth', 'timestamp_rgb', 'timestamp_gt']
-        
-        
-class DISCOMANParser(BaseParser):
+                 trajectory_dir,
+                 src_dir):
+        super(RetailBotParser, self).__init__(trajectory_dir, src_dir)
+        self.gt_txt_path = os.path.join(self.src_dir, 'pose.txt')
+        self.depth_txt_path = os.path.join(self.src_dir, 'depth.txt')
+        self.rgb_txt_path = os.path.join(self.src_dir, 'rgb.txt')
+        self.skiprows = 0
+
+    def __repr__(self):
+        return 'RetailBotParser(dir={}, txt_path={})'.format(self.dir, self.gt_txt_path)
+
+
+class DISCOMANParser(ElementwiseParser):
+
     def __init__(self,
-                 sequence_directory,
+                 trajectory_dir,
                  json_path):
-        super(DISCOMANParser, self).__init__(sequence_directory)
-        self.directory = os.path.dirname(json_path)
-        self.image_directory = os.path.dirname(json_path)
-        self.depth_directory = os.path.dirname(json_path)
+        super(DISCOMANParser, self).__init__(trajectory_dir)
+        self.src_dir = os.path.dirname(json_path)
         self.json_path = json_path
 
     def _load_data(self):
         with open(self.json_path) as read_file:
             data = json.load(read_file)
-        self.trajectory = data['trajectory']['frames']
+        self.trajectory = data['trajectory']['frames'][::5]
 
     @staticmethod    
     def get_path_to_rgb(item):
@@ -166,44 +163,27 @@ class DISCOMANParser(BaseParser):
         return item['id']
 
     @staticmethod
-    def get_global_quaternion(item):
+    def get_quaternion(item):
         return item['state']['global']['orientation']
 
     @staticmethod
-    def get_global_translation(item):
+    def get_translation(item):
         return item['state']['global']['position']
 
-    @staticmethod
-    def get_global_pose_matrix(item):
-        return pyquaternion.Quaternion(item['state']['global']['orientation']).rotation_matrix
-
-    def _create_global_dataframe(self):
-        trajectory_parsed = []
-
-        for point in self.trajectory[::5]:
-            parsed_point = {}
-            parsed_point['timestamp'] = self.get_timestamp(point)
-            parsed_point['path_to_rgb'] = self.get_path_to_rgb(point)
-            parsed_point['path_to_depth'] = self.get_path_to_depth(point)
-            parsed_point.update(dict(zip(['q_w', 'q_x', 'q_y', 'q_z'], self.get_global_quaternion(point))))
-            parsed_point.update(dict(zip(['t_x', 't_y', 't_z'], self.get_global_translation(point))))
-            trajectory_parsed.append(parsed_point)
-
-        self.global_dataframe = pd.DataFrame.from_dict(trajectory_parsed)
-
-    def _calculate_global_pose_matrices(self):
-        self.global_pose_matrices = np.array([self.get_global_pose_matrix(item) for item in self.trajectory])
+    def _parse_item(self, item):
+        parsed_item = {}
+        parsed_item['timestamp'] = self.get_timestamp(item)
+        parsed_item['path_to_rgb'] = self.get_path_to_rgb(item)
+        parsed_item['path_to_depth'] = self.get_path_to_depth(item)
+        parsed_item.update(dict(zip(['q_w', 'q_x', 'q_y', 'q_z'], self.get_quaternion(item))))
+        parsed_item.update(dict(zip(['t_x', 't_y', 't_z'], self.get_translation(item))))
+        return parsed_item
 
     def __repr__(self):
-        return 'JSONParser(dir={}, json_path={})'.format(self.sequence_directory, self.json_path)
+        return 'DISCOMANParser(dir={}, json_path={})'.format(self.dir, self.json_path)
     
     
 class OldDISCOMANParser(DISCOMANParser):
-    def __init__(self,
-                 sequence_directory,
-                 json_path):
-        super(OldDISCOMANParser, self).__init__(sequence_directory,
-                                                json_path)
 
     def _load_data(self):
         with open(self.json_path) as read_file:
@@ -223,69 +203,68 @@ class OldDISCOMANParser(DISCOMANParser):
         return item['time']
 
     @staticmethod
-    def get_global_quaternion(item):
+    def get_quaternion(item):
         return item['info']['agent_state']['orientation']
 
     @staticmethod
-    def get_global_translation(item):
+    def get_translation(item):
         return item['info']['agent_state']['position']
-
-    @staticmethod
-    def get_global_pose_matrix(item):
-        return pyquaternion.Quaternion(item['info']['agent_state']['orientation']).rotation_matrix
     
-    def _create_global_dataframe(self):
-        trajectory_parsed = []
-
-        for point in self.trajectory:
-            parsed_point = {}
-            parsed_point['timestamp'] = self.get_timestamp(point)
-            parsed_point['path_to_rgb'] = self.get_path_to_rgb(point)
-            parsed_point['path_to_depth'] = self.get_path_to_depth(point)
-            parsed_point.update(dict(zip(['q_w', 'q_x', 'q_y', 'q_z'], self.get_global_quaternion(point))))
-            parsed_point.update(dict(zip(['t_x', 't_y', 't_z'], self.get_global_translation(point))))
-            trajectory_parsed.append(parsed_point)
-
-        self.global_dataframe = pd.DataFrame.from_dict(trajectory_parsed)
+    def __repr__(self):
+        return 'OldDISCOMANParser(dir={}, json_path={})'.format(self.dir, self.json_path)
         
         
-class KITTIParser(BaseParser):
+class KITTIParser(ElementwiseParser):
+
     def __init__(self, 
-             sequence_directory,
-             seq_id, 
-             dataset_root='/dbstore/datasets/KITTI_odometry_2012/dataset/sequences'):
-        self.sequence_directory = sequence_directory
-        self.dataset_root = dataset_root
-        self.seq_id = seq_id
-        self.directory = os.path.join(self.dataset_root, self.seq_id)
+                 trajectory_dir,
+                 trajectory_id,
+                 dataset_root='/dbstore/datasets/KITTI_odometry_2012/dataset/sequences'):
+        super(KITTIParser, self).__init__(trajectory_dir)
+        self.src_dir = os.path.join(dataset_root, trajectory_id)
+        self.image_dir = os.path.join(self.src_dir, 'image_2')
+        self.pose_filepath = os.path.join(os.path.dirname(dataset_root), 'poses', '{}.txt'.format(trajectory_id))
         self.cols = ['path_to_rgb']
-        
-    def read_poses_file(self):
-        global_poses_matrixes = []
-        with open(os.path.join(self.dataset_root,
-                               '..', 'poses', '{}.txt'.format(self.seq_id))) as global_poses_fp:
-            for line in global_poses_fp:
+
+        np.allclose = partial(np.allclose, atol=1e-6)
+
+    def _load_poses(self):
+        self.pose_matrices = []
+        with open(self.pose_filepath) as pose_fp:
+            for line in pose_fp:
                 T_w_cam0 = np.fromstring(line, dtype=float, sep=' ')
                 T_w_cam0 = T_w_cam0.reshape(3, 4)
                 T_w_cam0 = np.vstack((T_w_cam0, [0, 0, 0, 1]))
-                global_poses_matrixes.append(T_w_cam0)
-        return global_poses_matrixes
+                self.pose_matrices.append(T_w_cam0)
+
+    def _load_image_filepaths(self):
+        self.image_filepaths = [os.path.join(self.image_dir, image_filename) \
+                                for image_filename in sorted(os.listdir(self.image_dir))]
 
     def _load_data(self):
-        root_to_rgb = os.path.join(self.dataset_root, self.seq_id, 'image_2')
-        self.left_cam_frames = sorted(os.listdir(root_to_rgb))
-        self.left_cam_frames = [os.path.join(root_to_rgb, img_name) for img_name in self.left_cam_frames]
-        assert len(self.read_poses_file()) == len(self.left_cam_frames)
-        
-    def _create_global_dataframe(self):
-        np.allclose = partial(np.allclose, atol=1e-6)
-        trajectory_parsed = []
-        for i, (pose, rgb) in enumerate(zip(self.read_poses_file(),self.left_cam_frames)):
-            global_quaternion = pyquaternion.Quaternion(matrix=pose).elements
-            global_translation = pose[:3, 3]
-            parsed_point = {}
-            parsed_point['path_to_rgb'] = rgb
-            parsed_point.update(dict(zip(['q_w', 'q_x', 'q_y', 'q_z'], global_quaternion)))
-            parsed_point.update(dict(zip(['t_x', 't_y', 't_z'], global_translation)))
-            trajectory_parsed.append(parsed_point)
-        self.global_dataframe = pd.DataFrame.from_dict(trajectory_parsed)
+        self._load_image_filepaths()
+        self._load_poses()
+        assert len(self.pose_matrices) == len(self.image_filepaths)
+        self.trajectory = list(zip(self.pose_matrices, self.image_filepaths))
+
+    @staticmethod
+    def get_quaternion(item):
+        rotation_matrix, translation = split_se3(item[0])
+        quaternion = pyquaternion.Quaternion(matrix=rotation_matrix).elements
+        return quaternion
+
+    @staticmethod
+    def get_translation(item):
+        rotation_matrix, translation = split_se3(item[0])
+        return translation
+
+    @staticmethod
+    def get_path_to_rgb(item):
+        return item[1]
+
+    def _parse_item(self, item):
+        parsed_item = {}
+        parsed_item['path_to_rgb'] = self.get_path_to_rgb(item)
+        parsed_item.update(dict(zip(['q_w', 'q_x', 'q_y', 'q_z'], self.get_quaternion(item))))
+        parsed_item.update(dict(zip(['t_x', 't_y', 't_z'], self.get_translation(item))))
+        return parsed_item
