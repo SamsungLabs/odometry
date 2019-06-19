dc9bcab0732c1080ea2f0ccceff8fedd0aadab86
d-zhukov <d.zhukov@samsung.com>
2019-06-19 11:59:58 +0300

    refactoring

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -17,7 +17,7 @@ class TestDatasets(unittest.TestCase):
 
     def prepare_estimators(self, height, width):
 
-        weights_dir_path = Path(env.PROJECT_PATH)/"weights"
+        weights_dir_path = Path(env.PROJECT_PATH)/'weights'
         weights_file = 'pwcnet.ckpt-595000'
         self.assertTrue(list(weights_dir_path.glob(weights_file + '*')))
         optical_flow_checkpoint = (weights_dir_path/weights_file).as_posix()
@@ -55,22 +55,22 @@ class TestDatasets(unittest.TestCase):
 
     def assert_df(self, df, trajecotry_dir, num_files):
 
-        self.assertTrue(len(df["path_to_rgb"]) == (num_files - 1), )
-        for path in df["path_to_rgb"]:
+        self.assertTrue(len(df['path_to_rgb']) == (num_files - 1), )
+        for path in df['path_to_rgb']:
             self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, path)))
         self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, df['path_to_rgb_next'].iloc[-1])))
 
-        self.assertTrue(len(df["path_to_depth"]) == (num_files - 1))
-        for path in df["path_to_depth"]:
+        self.assertTrue(len(df['path_to_depth']) == (num_files - 1))
+        for path in df['path_to_depth']:
             self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, path)))
         self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, df['path_to_depth_next'].iloc[-1])))
 
-        self.assertTrue(len(df["path_to_optical_flow"]) == (num_files - 1))
-        for path in df["path_to_optical_flow"]:
+        self.assertTrue(len(df['path_to_optical_flow']) == (num_files - 1))
+        for path in df['path_to_optical_flow']:
             self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, path)))
 
     def test_tum(self) -> None:
-        print("Started TUM test")
+        print('Started TUM test')
 
         trajectory_dir = 'tum'
         dir = os.path.join(env.DATASET_PATH, 'tum_rgbd_flow/data/rgbd_dataset_freiburg2_coke')
@@ -88,7 +88,7 @@ class TestDatasets(unittest.TestCase):
         self.assert_df(df, trajectory_dir, num_files=num_files)
 
     def test_discoman(self) -> None:
-        print("Started DISCOMAN test")
+        print('Started DISCOMAN test')
 
         trajectory_dir = 'discoman'
         json_path = os.path.join(env.DATASET_PATH, 'renderbox/iros2019/dset/output/deprecated/000001/0_traj.json')
@@ -106,11 +106,11 @@ class TestDatasets(unittest.TestCase):
         self.assert_df(df, trajectory_dir, num_files=num_files)
 
     def test_kitti(self) -> None:
-        print("Started KITTI test")
+        print('Started KITTI test')
         trajectory_dir = 'kitti'
         trajectory_id = '00'
         height, width = 94, 300
-        dataset_root = os.path.join(env.DATASET_PATH, "KITTI_odometry_2012/dataset/sequences")
+        dataset_root = os.path.join(env.DATASET_PATH, 'KITTI_odometry_2012/dataset/sequences')
         parser = KITTIParser(trajectory_dir, trajectory_id=trajectory_id, dataset_root=dataset_root)
         num_files = 10
 
@@ -124,7 +124,7 @@ class TestDatasets(unittest.TestCase):
         self.assert_df(df, trajectory_dir, num_files=num_files)
 
     def test_retailbot(self) -> None:
-        print("Started RetailBot test")
+        print('Started RetailBot test')
 
         trajectory_dir = 'retailbot'
         dir = os.path.join(env.DATASET_PATH, 'retail_bot/meetingroom_04_rgbd_ir_imu_pose')
