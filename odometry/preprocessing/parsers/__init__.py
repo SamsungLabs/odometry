3e5f78abbdd732bdc7e98485aa3d5de7cacc0398
f.konokhov <f.konokhov@partner.samsung.com>
2019-06-20 19:04:32 +0300

    added saic_office parser

--- odometry/preprocessing/parsers/__init__.py
+++ odometry/preprocessing/parsers/__init__.py
@@ -1,17 +1,21 @@
 from .tum_parser import TUMParser
 
 from .retailbot_parser import RetailBotParser
+from .retailbot_parser import SAICOfficeParser
 
 from .kitti_parser import KITTIParser
 
 from .discoman_parser import DISCOMANParser
 from .discoman_parser import OldDISCOMANParser
+from .discoman_parser import DISCOMANCSVParser
 
 
 __all__ = [
     'KITTIParser',
     'TUMParser',
     'RetailBotParser',
+    'SAICOfficeParser',
     'DISCOMANParser',
-    'OldDISCOMANParser'
+    'OldDISCOMANParser',
+    'DISCOMANCSVParser'    
 ]
--- odometry/preprocessing/parsers/retailbot_parser.py
+++ odometry/preprocessing/parsers/retailbot_parser.py
@@ -16,3 +16,23 @@ class RetailBotParser(TUMParser):
 
     def __repr__(self):
         return 'RetailBotParser(dir={}, txt_path={})'.format(self.dir, self.gt_txt_path)
+
+    
+    
+class SAICOfficeParser(RetailBotParser):
+    
+    def __init__(self,
+                 trajectory_dir,
+                 src_dir):
+        super(SAICOfficeParser, self).__init__(trajectory_dir, src_dir)
+        self.cols = ['path_to_rgb']
+        
+    def _load_data(self):
+        self.dataframes = [ self._load_rgb_txt(), self._load_gt_txt()]
+        self.timestamp_cols = ['timestamp_rgb', 'timestamp_gt']
+        
+    def _load_depth_txt(self):
+        pass
+        
+    def __repr__(self):
+        return 'SAICOfficeParser(dir={}, txt_path={})'.format(self.dir, self.gt_txt_path)
