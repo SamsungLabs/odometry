ef134a417b419ea50ff404e15d0f7542912e62f6
a.vorontsova <a.vorontsova@samsung.com>
2019-06-24 15:42:52 +0300

    Added test subset to generator

--- odometry/data_manager/generator_factory.py
+++ odometry/data_manager/generator_factory.py
@@ -14,17 +14,20 @@ class GeneratorFactory:
                  csv_name,
                  train_trajectories=None,
                  val_trajectories=None,
+                 test_trajectories=None,
                  x_col=('path_to_rgb', 'path_to_rgb_next'),
                  y_col=('euler_x', 'euler_y', 'euler_z', 't_x', 't_y', 't_z'),
                  image_col=('path_to_rgb', 'path_to_rgb_next'),
                  train_generator_args=None,
                  val_generator_args=None,
+                 test_generator_args=None,
                  validate_on_train_trajectory=False,
                  val_ratio=0.0,
                  number_of_folds=None,
                  fold_index=0,
                  train_sampling_step=1,
                  val_sampling_step=1,
+                 test_sampling_step=1,
                  batch_size=128,
                  cached_images=None,
                  *args, **kwargs):
@@ -45,10 +48,12 @@ class GeneratorFactory:
 
         self.train_trajectories = train_trajectories
         self.val_trajectories = val_trajectories
+        self.test_trajectories = test_trajectories
 
         self.df_train = self._get_multi_df_dataset(self.train_trajectories) \
-                            if self.train_trajectories else None
+            if self.train_trajectories else None
         self.df_val = self._get_multi_df_dataset(self.val_trajectories)
+        self.df_test = self._get_multi_df_dataset(self.test_trajectories)
 
         if number_of_folds is not None:
             val_ratio = 1. / number_of_folds
@@ -61,18 +66,13 @@ class GeneratorFactory:
             self.df_train = pd.concat([self.df_train[:start], self.df_train[end:]])
             self.df_val = self.df_val[start:end]
 
-        if train_sampling_step != 1:
-            self.df_train = self.df_train.iloc[::train_sampling_step]
-        if val_sampling_step != 1:
-            self.df_val = self.df_val.iloc[::val_sampling_step]
+        self.df_train = self.df_train.iloc[::train_sampling_step]
+        self.df_val = self.df_val.iloc[::val_sampling_step]
+        self.df_test = self.df_test.iloc[::test_sampling_step]
 
-        if train_generator_args is None:
-            train_generator_args = {}
-        if val_generator_args is None:
-            val_generator_args = {}
-
-        self.train_generator_args = train_generator_args
-        self.val_generator_args = val_generator_args
+        self.train_generator_args = train_generator_args or {}
+        self.val_generator_args = val_generator_args or {}
+        self.test_generator_args = test_generator_args or {}
 
         self.args = args
         self.kwargs = kwargs
@@ -81,10 +81,8 @@ class GeneratorFactory:
         if type(self.cached_images) == str:
             self.load_cache(self.cached_images)
 
-        if self.df_train is not None:
-            self.input_shapes = self.get_train_generator().input_shapes
-        else:
-            self.input_shapes = self.get_val_generator().input_shapes
+        self.input_shapes = self.get_train_generator().input_shapes \
+            if self.train_trajectories else self.get_val_generator().input_shapes
 
     def _get_multi_df_dataset(self, trajectories):
         df = None
@@ -92,26 +90,19 @@ class GeneratorFactory:
             current_df = pd.read_csv(os.path.join(self.dataset_root, trajectory_name, self.csv_name))
             current_df[self.image_col] = trajectory_name + '/' + current_df[self.image_col]
             current_df['trajectory_id'] = trajectory_name
-            if df is None:
-                df = current_df
-            else:
-                df = df.append(current_df, sort=False)
+            df = df.append(current_df, sort=False) if df is not None else current_df
 
         df.index = range(len(df))
         return df
 
     def warm_up_cache(self):
         assert self.cached_images is not None
-
-        print('Train:', flush=True)
-        train_generator = self.get_train_generator()
-        for batch_index in tqdm(range(len(train_generator))):
-            train_generator[batch_index]
-
-        print('Validation:', flush=True)
-        val_generator = self.get_val_generator()
-        for batch_index in tqdm(range(len(val_generator))):
-            val_generator[batch_index]
+        for subset, generator in (('Train', self.get_train_generator()),
+                                  ('Val', self.get_val_generator()),
+                                  ('Test', self.get_test_generator())):
+            print(subset, flush=True)
+            for batch_index in tqdm(range(len(generator))):
+                generator[batch_index]
 
     def load_cache(self, cache_file):
         try:
@@ -156,3 +147,6 @@ class GeneratorFactory:
 
     def get_val_generator(self):
         return self._get_generator(self.df_val, self.val_generator_args, trajectory=True)
+
+    def get_test_generator(self):
+        return self._get_generator(self.df_test, self.test_generator_args, trajectory=True)
