b5bd011da4279b81183ebb9f0e47257ff3ff4efa
d-zhukov <d.zhukov@samsung.com>
2019-06-26 16:10:55 +0300

    Wrote script for preparing dataset. It can be called from python script and from terminal 1. Config to log weights of networks 2. Added logger to log trajectories 3. Rewrote test_dataset to use this script

---
+++ odometry/preprocessing/prepare_dataset.py
@@ -0,0 +1,147 @@
+import os
+import argparse
+from . import __init_path__
+import env
+import json
+from odometry.preprocessing import parsers, estimators, prepare_trajectory
+from tqdm import tqdm
+from pathlib import Path
+from odometry.utils.computation_utils import limit_resources
+import logging
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def initialize_estimators(target_size, optical_flow_checkpoint, depth_checkpoint=None):
+
+    single_frame_estimators = list()
+
+    quaternion2euler_estimator = estimators.Quaternion2EulerEstimator(input_col=['q_w', 'q_x', 'q_y', 'q_z'],
+                                                                      output_col=['euler_x', 'euler_y', 'euler_z'])
+    single_frame_estimators.append(quaternion2euler_estimator)
+
+    if depth_checkpoint:
+        struct2depth_estimator = estimators.Struct2DepthEstimator(input_col='path_to_rgb',
+                                                                  output_col='path_to_depth',
+                                                                  sub_dir='depth',
+                                                                  checkpoint=depth_checkpoint,
+                                                                  height=target_size[0],
+                                                                  width=target_size[1])
+        single_frame_estimators.append(struct2depth_estimator
+                                       )
+    cols = ['euler_x', 'euler_y', 'euler_z', 't_x', 't_y', 't_z']
+    input_col = cols + [col + '_next' for col in cols]
+    output_col = cols
+    global2relative_estimator = estimators.Global2RelativeEstimator(input_col=input_col,
+                                                                    output_col=output_col)
+
+    pwcnet_estimator = estimators.PWCNetEstimator(input_col=['path_to_rgb', 'path_to_rgb_next'],
+                                                  output_col='path_to_optical_flow',
+                                                  sub_dir='optical_flow',
+                                                  checkpoint=optical_flow_checkpoint)
+
+    pair_frames_estimators = [global2relative_estimator, pwcnet_estimator]
+    return single_frame_estimators, pair_frames_estimators
+
+
+def initialize_parser(dataset_type):
+    if dataset_type == "kitti":
+        return parsers.KITTIParser
+    elif dataset_type == "discoman":
+        return parsers.DISCOMANJSONParser
+    elif dataset_type == "tum":
+        return parsers.TUMParser
+    elif dataset_type == "retailbot":
+        return parsers.RetailBotParser
+    else:
+        raise RuntimeError("Unexpected dataset type")
+
+
+def get_all_trajectories(dataset_root):
+
+    if not isinstance(dataset_root, Path):
+        dataset_root = Path(dataset_root)
+
+    logger = logging.getLogger("prepare_dataset")
+
+    trajectories = list()
+    for d in dataset_root.rglob("**/*"):
+        if list(d.glob("*traj.json")) or\
+                list(d.glob("rgb.txt")) or \
+                list(d.glob("image_2")) or \
+                list(d.glob("camera_gt.csv")):
+            logger.info(f"Trajectory {d.as_posix()} added")
+            trajectories.append(d.as_posix())
+
+    return trajectories
+
+
+def set_logger(output_dir):
+    fh = logging.FileHandler(output_dir.joinpath("log.txt").as_posix(), mode="w+")
+    fh.setLevel(logging.DEBUG)
+
+    logger = logging.getLogger("prepare_dataset")
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(fh)
+
+
+def prepare_dataset(dataset_type, dataset_root, output_dir, target_size, optical_flow_checkpoint,
+                    depth_checkpoint=None):
+
+    limit_resources()
+
+    if not isinstance(output_dir, Path):
+        output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    set_logger(output_dir)
+
+    sf_estimators, pf_estimators = initialize_estimators(target_size,
+                                                         optical_flow_checkpoint=optical_flow_checkpoint,
+                                                         depth_checkpoint=depth_checkpoint)
+
+    parser_class = initialize_parser(dataset_type)
+    trajectories = get_all_trajectories(dataset_root)
+
+    with open(output_dir.joinpath("config.json").as_posix(), mode="w+") as f:
+        dataset_config = {"depth_checkpoint": depth_checkpoint, "optical_flow_checkpoint": optical_flow_checkpoint}
+        json.dump(dataset_config, f)
+
+    for trajectory in tqdm(trajectories):
+        trajectory_parser = parser_class(trajectory)
+        trajectory_name = os.path.basename(trajectory)
+        output_dir = output_dir.joinpath(trajectory_name)
+        df = prepare_trajectory(output_dir.as_posix(),
+                                parser=trajectory_parser,
+                                single_frame_estimators=sf_estimators,
+                                pair_frames_estimators=pf_estimators,
+                                stride=1)
+        df.to_csv(output_dir.joinpath('df.csv').as_posix(), index=False)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, help="possible variants: kitti, discoman, tum, retailbot")
+    parser.add_argument("--dataset_root", type=str)
+    parser.add_argument("--output_dir", type=str)
+    parser.add_argument("--of_checkpoint", type=str,
+                        default='/Vol0/user/f.konokhov/tfoptflow/tfoptflow/tmp/pwcnet.ckpt-84000')
+    parser.add_argument("--depth", type=str2bool, default=True)
+    parser.add_argument("--depth_checkpoint", type=str,
+                        default=os.path.join(env.PROJECT_PATH, 'weights/model-199160'))
+    parser.add_argument("--target_size", type=int, nargs="+")
+    args = parser.parse_args()
+
+    prepare_dataset(args.dataset,
+                    dataset_root=args.dataset_root,
+                    output_dir=args.output_dir,
+                    target_size=args.target_size,
+                    optical_flow_checkpoint=args.of_checkpoint,
+                    depth_checkpoint=args.depth_checkpoint if args.depth else None)
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,139 +3,118 @@ import __init_path__
 import env
 import os
 from pathlib import Path
-from odometry.preprocessing.parsers import (KITTIParser,
-                                            TUMParser,
-                                            RetailBotParser,
-                                            DISCOMANJSONParser)
-from odometry.preprocessing import estimators as est
-from odometry.preprocessing.prepare_trajectory import prepare_trajectory
+import pandas as pd
+import shutil
+
+from odometry.preprocessing.prepare_dataset import prepare_dataset
 
 env.DATASET_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'minidataset')
 
 
 class TestDatasets(unittest.TestCase):
 
-    def prepare_estimators(self, height, width):
+    def setUp(self) -> None:
+        self.output_dir = Path(env.PROJECT_PATH).joinpath('tmp')
+        shutil.rmtree(self.output_dir.as_posix(), ignore_errors=True)
 
-        weights_dir_path = Path(env.PROJECT_PATH)/'weights'
-        weights_file = 'pwcnet.ckpt-595000'
-        self.assertTrue(list(weights_dir_path.glob(weights_file + '*')))
-        optical_flow_checkpoint = (weights_dir_path/weights_file).as_posix()
+    def tearDown(self) -> None:
+        shutil.rmtree(self.output_dir.as_posix())
 
-        weights_file = 'model-199160'
-        self.assertTrue(list(weights_dir_path.glob(weights_file + '*')))
-        depth_checkpoint = (weights_dir_path/weights_file).as_posix()
-
-        quaternion2euler_estimator = est.Quaternion2EulerEstimator(input_col=['q_w', 'q_x', 'q_y', 'q_z'],
-                                                                   output_col=['euler_x', 'euler_y', 'euler_z'])
-
-        struct2depth_estimator = est.Struct2DepthEstimator(input_col='path_to_rgb',
-                                                           output_col='path_to_depth',
-                                                           sub_dir='depth',
-                                                           checkpoint=depth_checkpoint,
-                                                           height=height,
-                                                           width=width)
-
-        input_col = ['euler_x', 'euler_y', 'euler_z', 't_x', 't_y', 't_z']
-        input_col.extend([col + '_next' for col in input_col])
-        output_col = input_col
-        global2relative_estimator = est.Global2RelativeEstimator(input_col=input_col, output_col=output_col)
-
-        pwcnet_estimator = est.PWCNetEstimator(input_col=['path_to_rgb', 'path_to_rgb_next'],
-                                               output_col='path_to_optical_flow',
-                                               sub_dir='optical_flow',
-                                               checkpoint=optical_flow_checkpoint,
-                                               height=height,
-                                               width=width)
-
-        single_frame_estimators = [quaternion2euler_estimator, struct2depth_estimator]
-        pair_frames_estimators = [global2relative_estimator, pwcnet_estimator]
-
-        return single_frame_estimators, pair_frames_estimators
-
-    def assert_df(self, df, trajecotry_dir, num_files):
+    def assert_df(self, df, trajectory_dir, num_files):
 
         self.assertTrue(len(df['path_to_rgb']) == (num_files - 1), )
         for path in df['path_to_rgb']:
-            self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, path)))
-        self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, df['path_to_rgb_next'].iloc[-1])))
+            self.assertTrue(os.path.isfile(os.path.join(trajectory_dir, path)))
+        self.assertTrue(os.path.isfile(os.path.join(trajectory_dir, df['path_to_rgb_next'].iloc[-1])))
 
         self.assertTrue(len(df['path_to_depth']) == (num_files - 1))
         for path in df['path_to_depth']:
-            self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, path)))
-        self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, df['path_to_depth_next'].iloc[-1])))
+            self.assertTrue(os.path.isfile(os.path.join(trajectory_dir, path)))
+        self.assertTrue(os.path.isfile(os.path.join(trajectory_dir, df['path_to_depth_next'].iloc[-1])))
 
         self.assertTrue(len(df['path_to_optical_flow']) == (num_files - 1))
         for path in df['path_to_optical_flow']:
-            self.assertTrue(os.path.isfile(os.path.join(trajecotry_dir, path)))
+            self.assertTrue(os.path.isfile(os.path.join(trajectory_dir, path)))
 
     def test_tum(self) -> None:
         print('Started TUM test')
 
-        output_dir = 'tum'
-        trajectory_dir = os.path.join(env.DATASET_PATH, 'tum_rgbd_flow/data/rgbd_dataset_freiburg2_coke')
-        height, width = 480, 640
         num_files = 7
-        parser = TUMParser(trajectory_dir)
+        prepare_dataset(dataset_type='tum',
+                        dataset_root=os.path.join(env.DATASET_PATH, 'tum_rgbd_flow'),
+                        output_dir=self.output_dir.as_posix(),
+                        target_size=(120, 160),
+                        optical_flow_checkpoint=os.path.join(env.PROJECT_PATH, 'weights/pwcnet.ckpt-595000'),
+                        depth_checkpoint=os.path.join(env.PROJECT_PATH, 'weights/model-199160')
+                        )
 
-        single_frame_estimators, pair_frames_estimators = self.prepare_estimators(height=height, width=width)
-        df = prepare_trajectory(output_dir,
-                                parser=parser,
-                                single_frame_estimators=single_frame_estimators,
-                                pair_frames_estimators=pair_frames_estimators,
-                                stride=1)
+        csv_path = list(self.output_dir.rglob("*.csv"))
+        self.assertTrue(len(csv_path) == 1)
 
-        self.assert_df(df, output_dir, num_files=num_files)
+        csv_path = csv_path[0]
+        trajectory_dir = csv_path.parent
+        df = pd.read_csv(csv_path)
+        self.assert_df(df, trajectory_dir=trajectory_dir, num_files=num_files)
 
     def test_discoman(self) -> None:
         print('Started DISCOMAN test')
 
-        output_dir = 'discoman'
-        trajectory_dir = os.path.join(env.DATASET_PATH, 'renderbox/iros2019/dset/output/deprecated/000001')
-        height, width = 120, 160
-        parser = DISCOMANJSONParser(trajectory_dir)
         num_files = 5
 
-        single_frame_estimators, pair_frames_estimators = self.prepare_estimators(height=height, width=width)
-        df = prepare_trajectory(output_dir,
-                                parser=parser,
-                                single_frame_estimators=single_frame_estimators,
-                                pair_frames_estimators=pair_frames_estimators,
-                                stride=1)
+        prepare_dataset(dataset_type='discoman',
+                        dataset_root=os.path.join(env.DATASET_PATH, 'renderbox'),
+                        output_dir=self.output_dir.as_posix(),
+                        target_size=(120, 160),
+                        optical_flow_checkpoint=os.path.join(env.PROJECT_PATH, 'weights/pwcnet.ckpt-595000'),
+                        depth_checkpoint=os.path.join(env.PROJECT_PATH, 'weights/model-199160')
+                        )
 
-        self.assert_df(df, output_dir, num_files=num_files)
+        csv_path = list(self.output_dir.rglob("*.csv"))
+        self.assertTrue(len(csv_path) == 1)
+
+        csv_path = csv_path[0]
+        trajectory_dir = csv_path.parent
+        df = pd.read_csv(csv_path)
+        self.assert_df(df, trajectory_dir=trajectory_dir, num_files=num_files)
 
     def test_kitti(self) -> None:
         print('Started KITTI test')
-        output_dir = 'kitti'
-        height, width = 94, 300
-        trajectory_dir = os.path.join(env.DATASET_PATH, 'KITTI_odometry_2012/dataset/sequences/00')
-        parser = KITTIParser(trajectory_dir)
+
         num_files = 10
 
-        single_frame_estimators, pair_frames_estimators = self.prepare_estimators(height=height, width=width)
-        df = prepare_trajectory(output_dir,
-                                parser=parser,
-                                single_frame_estimators=single_frame_estimators,
-                                pair_frames_estimators=pair_frames_estimators,
-                                stride=1)
+        prepare_dataset(dataset_type='kitti',
+                        dataset_root=os.path.join(env.DATASET_PATH, 'KITTI_odometry_2012'),
+                        output_dir=self.output_dir.as_posix(),
+                        target_size=(120, 160),
+                        optical_flow_checkpoint=os.path.join(env.PROJECT_PATH, 'weights/pwcnet.ckpt-595000'),
+                        depth_checkpoint=os.path.join(env.PROJECT_PATH, 'weights/model-199160')
+                        )
 
-        self.assert_df(df, output_dir, num_files=num_files)
+        csv_path = list(self.output_dir.rglob("*.csv"))
+        self.assertTrue(len(csv_path) == 1)
+
+        csv_path = csv_path[0]
+        trajectory_dir = csv_path.parent
+        df = pd.read_csv(csv_path)
+        self.assert_df(df, trajectory_dir=trajectory_dir, num_files=num_files)
 
     def test_retailbot(self) -> None:
         print('Started RetailBot test')
 
-        output_dir = 'retailbot'
-        trajectory_dir = os.path.join(env.DATASET_PATH, 'retail_bot/meetingroom_04_rgbd_ir_imu_pose')
-        height, width = 480, 640
-        parser = RetailBotParser(trajectory_dir)
         num_files = 2
 
-        single_frame_estimators, pair_frames_estimators = self.prepare_estimators(height=height, width=width)
-        df = prepare_trajectory(output_dir,
-                                parser=parser,
-                                single_frame_estimators=single_frame_estimators,
-                                pair_frames_estimators=pair_frames_estimators,
-                                stride=1)
+        prepare_dataset(dataset_type='retailbot',
+                        dataset_root=os.path.join(env.DATASET_PATH, 'retail_bot'),
+                        output_dir=self.output_dir.as_posix(),
+                        target_size=(120, 160),
+                        optical_flow_checkpoint=os.path.join(env.PROJECT_PATH, 'weights/pwcnet.ckpt-595000'),
+                        depth_checkpoint=os.path.join(env.PROJECT_PATH, 'weights/model-199160')
+                        )
 
-        self.assert_df(df, output_dir, num_files=num_files)
+        csv_path = list(self.output_dir.rglob("*.csv"))
+        self.assertTrue(len(csv_path) == 1)
+
+        csv_path = csv_path[0]
+        trajectory_dir = csv_path.parent
+        df = pd.read_csv(csv_path)
+        self.assert_df(df, trajectory_dir=trajectory_dir, num_files=num_files)
