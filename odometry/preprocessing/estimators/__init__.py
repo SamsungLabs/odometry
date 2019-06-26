1024c1d9e233fe086a887859e5fbefcd59aa6c1d
f.konokhov <f.konokhov@partner.samsung.com>
2019-06-26 16:04:58 +0300

    added feature extractor estimator

--- odometry/preprocessing/estimators/__init__.py
+++ odometry/preprocessing/estimators/__init__.py
@@ -2,4 +2,5 @@ from odometry.preprocessing.estimators.quaternion2euler_estimator import Quatern
 from odometry.preprocessing.estimators.global2relative_estimator import Global2RelativeEstimator
 from odometry.preprocessing.estimators.struct2depth_estimator import Struct2DepthEstimator
 from odometry.preprocessing.estimators.pwcnet_estimator import PWCNetEstimator
+from odometry.preprocessing.estimators.pwcnet_feature_extractor import PWCNetFeatureExtractor
 #from odometry.preprocessing.estimators.senet_estimator import SENetEstimator
