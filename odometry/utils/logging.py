3b55cf7a802c9489704aa1b286d91da2262a8201
d-zhukov <d.zhukov@samsung.com>
2019-07-09 14:25:39 +0300

    Added simple decorator for mlflow logging

---
+++ odometry/utils/logging.py
@@ -0,0 +1,58 @@
+import inspect
+import mlflow
+
+
+def mlflow_logging(func):
+
+    def initialize(arg_spec):
+        params = {k: None for k in arg_spec.args}
+
+        return params
+
+    def log_default(params, arg_spec):
+        for i in range(len(arg_spec.defaults)):
+            params[arg_spec.args[-(i + 1)]] = arg_spec.defaults[-(i + 1)]
+
+        return params
+
+    def log_input(params, arg_spec, args, kwargs):
+        for i in range(len(args)):
+            params[arg_spec.args[i]] = args[i]
+
+        for k, v in kwargs.items():
+            params[k] = v
+
+        return params
+
+    def filter_ignore(params, kwargs):
+        print(params)
+        ignore = params.get('ignore', list())
+        for k in ignore:
+            params.pop(k, None)
+
+        params.pop('ignore', None)
+
+        return params
+
+    def log(*args, **kwargs):
+
+        # if not mlflow.active_run():
+        #     return func(*args, **kwargs)
+
+        print(args)
+        print(kwargs)
+
+        arg_spec = inspect.getargspec(func)
+
+        params = initialize(arg_spec)
+        params = log_default(params, arg_spec)
+        params = log_input(params, arg_spec, args, kwargs)
+        params = filter_ignore(params, kwargs)
+
+        print(params)
+
+        mlflow.log_params(params)
+
+        func(*args, **kwargs)
+
+    return log
