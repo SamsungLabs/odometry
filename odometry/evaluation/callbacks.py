afcd5c1e22cdf0d8dda5e78689ce0719cb6cc5de
a.vorontsova <a.vorontsova@samsung.com>
2019-07-09 21:50:55 +0300

    Fixed saving predictions with confidence

--- odometry/evaluation/callbacks.py
+++ odometry/evaluation/callbacks.py
@@ -33,6 +33,19 @@ def on_batch_end(cls, batch, logs=None):
 keras.callbacks.ProgbarLogger.on_batch_end = on_batch_end
 
 
+def on_train_end(self, logs=None):
+    logs = logs or {}
+    file_ext = os.path.splitext(self.filepath)[-1]
+    file_path = os.path.join(os.path.dirname(self.filepath), 'final' + file_ext)
+    if self.save_weights_only:
+        self.model.save_weights(file_path, overwrite=True)
+    else:
+        self.model.save(file_path, overwrite=True)
+
+
+keras.callbacks.ModelCheckpoint.on_train_end = on_train_end
+
+
 class TerminateOnLR(keras.callbacks.Callback):
     '''Stop training when a lr has become less than passed min_lr.
     # Arguments
@@ -51,7 +64,7 @@ class TerminateOnLR(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs=None):
         lr = K.get_value(self.model.optimizer.lr)
-        mlflow.log_metric('lr', lr, step=epoch) if mlflow.active_run() else None
+        mlflow.log_metric('lr', float(lr), step=epoch) if mlflow.active_run() else None
         if lr < self.min_lr:
             self.stopped_epoch = epoch
             self.model.stop_training = True
@@ -62,19 +75,29 @@ class TerminateOnLR(keras.callbacks.Callback):
             print(f'Epoch {self.stopped_epoch}: terminated on lr = {lr} < {self.min_lr}')
 
 
+class MLFlowLogger(keras.callbacks.Callback):
+
+    def on_epoch_end(self, epoch, logs=None):
+        train_loss = logs['loss']
+        val_loss = logs.get('val_loss', np.inf)
+        if mlflow.active_run():
+            mlflow.log_metric('train_loss', train_loss, step=epoch)
+            mlflow.log_metric('val_loss', val_loss, step=epoch)
+
+
 class Evaluate(keras.callbacks.Callback):
     def __init__(self,
                  model,
                  dataset,
-                 predictions_dir=None,
-                 visuals_dir=None,
-                 period=1,
+                 run_dir=None,
+                 artifact_dir=None,
+                 period=10,
                  save_best_only=True,
                  max_to_visualize=5):
         super(Evaluate, self).__init__()
         self.model = model
-        self.predictions_dir = predictions_dir
-        self.visuals_dir = visuals_dir
+        self.run_dir = run_dir
+        self.artifact_dir = artifact_dir
 
         self.period = period
         self.epoch_counter = 0
@@ -92,13 +115,14 @@ class Evaluate(keras.callbacks.Callback):
 
         self.y_cols = self.train_generator.y_cols[:]
 
-        self.save_visuals = self.visuals_dir is not None
-        if self.save_visuals:
+        if self.run_dir:
+            self.visuals_dir = os.path.join(self.run_dir, 'visuals')
             os.makedirs(self.visuals_dir, exist_ok=True)
 
-        self.save_predictions = self.predictions_dir is not None
-        if self.save_predictions:
+            self.predictions_dir = os.path.join(self.run_dir, 'predictions')
             os.makedirs(self.predictions_dir, exist_ok=True)
+
+        self.save_artifacts = self.run_dir and self.artifact_dir
 
     def _create_visualization_file_path(self, prediction_id, subset, trajectory_id):
         file_path = os.path.join(self.visuals_dir, prediction_id, subset, f'{trajectory_id}.html')
@@ -113,17 +137,12 @@ class Evaluate(keras.callbacks.Callback):
     def _create_trajectory(self, df):
         return RelativeTrajectory.from_dataframe(df[self.y_cols]).to_global()
 
-    def _maybe_save_predictions(self, prediction_id, subset, trajectory_id, predictions):
-        if not self.save_predictions:
-            return
+    def _save_predictions(self, prediction_id, subset, trajectory_id, predictions):
         file_path = self._create_prediction_file_path(prediction_id, subset, trajectory_id)
         predictions.to_csv(file_path)
 
-    def _maybe_visualize_trajectory(self, prediction_id, subset, trajectory_id, predicted_trajectory,
-                                    gt_trajectory=None, trajectory_metrics=None):
-        if not self.save_visuals:
-            return
-
+    def _visualize_trajectory(self, prediction_id, subset, trajectory_id, predicted_trajectory,
+                              gt_trajectory=None, trajectory_metrics=None):
         file_path = self._create_visualization_file_path(prediction_id, subset, trajectory_id)
         if gt_trajectory is None:
             title = trajectory_id.upper()
@@ -133,7 +152,8 @@ class Evaluate(keras.callbacks.Callback):
             normalized_metrics_as_str = ', '.join([f'{key}: {value:.6f}'
                                                    for key, value in normalized_metrics.items()])
             title = f'{trajectory_id.upper()}: {normalized_metrics_as_str}'
-            visualize_trajectory_with_gt(gt_trajectory, predicted_trajectory, title=title, file_path=file_path)
+            visualize_trajectory_with_gt(gt_trajectory, predicted_trajectory,
+                                         title=title, file_path=file_path)
 
     def _evaluate(self, generator, gt, subset, prediction_id):
         if generator is None:
@@ -148,14 +168,14 @@ class Evaluate(keras.callbacks.Callback):
                                                                desc=f'Evaluate on {subset}')):
             gt_trajectory = self._create_trajectory(gt.iloc[indices])
             predicted_trajectory = self._create_trajectory(predictions.iloc[indices])
-            self._maybe_save_predictions(prediction_id, subset, trajectory_id, predictions.iloc[indices])
+            self._save_predictions(prediction_id, subset, trajectory_id, predictions.iloc[indices])
 
             trajectory_metrics = calculate_metrics(gt_trajectory, predicted_trajectory)
             records.append(trajectory_metrics)
 
             if i < self.max_to_visualize:
-                self._maybe_visualize_trajectory(prediction_id, subset, trajectory_id, predicted_trajectory,
-                                                 gt_trajectory, trajectory_metrics)
+                self._visualize_trajectory(prediction_id, subset, trajectory_id, predicted_trajectory,
+                                           gt_trajectory, trajectory_metrics)
 
         total_metrics = {f'{subset}_{key}': value for key, value in average_metrics(records).items()}
         return total_metrics
@@ -171,8 +191,8 @@ class Evaluate(keras.callbacks.Callback):
                                                 total=gt.trajectory_id.nunique(),
                                                 desc=f'Visualize {subset}'):
             predicted_trajectory = self._create_trajectory(predictions.iloc[indices])
-            self._maybe_save_predictions(prediction_id, subset, trajectory_id, predictions.iloc[indices])
-            self._maybe_visualize_trajectory(prediction_id, subset, trajectory_id, predicted_trajectory)
+            self._save_predictions(prediction_id, subset, trajectory_id, predictions.iloc[indices])
+            self._visualize_trajectory(prediction_id, subset, trajectory_id, predicted_trajectory)
 
     def _predict(self, generator, gt):
         generator.reset()
@@ -189,7 +209,7 @@ class Evaluate(keras.callbacks.Callback):
 
     def on_epoch_end(self, epoch, logs={}):
         self.epoch_counter += 1
-        if self.epoch_counter < self.period:
+        if self.period and self.epoch_counter < self.period:
             return
 
         self.epoch_counter = 0
@@ -206,13 +226,17 @@ class Evaluate(keras.callbacks.Callback):
         val_metrics = self._evaluate(self.val_generator, self.df_val, 'val', prediction_id)
 
         if mlflow.active_run():
-            mlflow.log_metric('train_loss', train_loss, step=epoch)
-            mlflow.log_metric('val_loss', val_loss, step=epoch)
             [mlflow.log_metric(key=key, value=value, step=epoch) for key, value in train_metrics.items()]
             [mlflow.log_metric(key=key, value=value, step=epoch) for key, value in val_metrics.items()]
+
+        if self.save_artifacts:
+            mlflow.log_artifacts(self.run_dir, self.artifact_dir)
 
     def on_train_end(self, logs={}):
         prediction_id = 'test'
         test_metrics = self._evaluate(self.test_generator, self.df_test, 'test', prediction_id)
-        mlflow.log_metrics(test_metrics) if mlflow.active_run() else None
+        mlflow.log_metrics(test_metrics)
+
         self._visualize(self.test_generator, self.df_test, 'test', 'test')
+        if self.save_artifacts:
+            mlflow.log_artifacts(self.run_dir, self.artifact_dir)
