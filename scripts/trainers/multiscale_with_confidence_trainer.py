8b04fe78c0f6144c188ac7558745a80b4005811d
a.vorontsova <a.vorontsova@samsung.com>
2019-07-11 17:20:30 +0300

    Added trainers for models with confidence

---
+++ scripts/trainers/multiscale_with_confidence_trainer.py
@@ -0,0 +1,52 @@
+import os
+from functools import partial
+
+import __init_path__
+import env
+
+from confidence_trainer import ConfidenceTrainer
+from odometry.models import construct_multiscale_model
+
+
+class MultiscaleWithConfidenceTrainer(ConfidenceTrainer):
+
+    def get_dataset(self,
+                    train_trajectories=None,
+                    val_trajectories=None,
+                    test_trajectories=None):
+        self.x_col = ['path_to_optical_flow']
+        self.image_col = ['path_to_optical_flow']
+        self.load_mode = ['flow_xy']
+        self.preprocess_mode = ['flow_xy']
+        return super().get_dataset(train_trajectories=train_trajectories,
+                                   val_trajectories=val_trajectories,
+                                   test_trajectories=test_trajectories)
+
+    def get_model_factory(self, input_shapes):
+        self.construct_model_fn = partial(construct_multiscale_model,
+                                          return_confidence=True)
+        self.lr = 0.001
+        self.loss = 'huber'
+        self.scale_rotation = 50
+        return super().get_model_factory(input_shapes)
+
+    def get_callbacks(self, model, dataset):
+        return super().get_callbacks(model=model,
+                                     dataset=dataset)
+
+    def fit_generator(self, model, dataset, epochs):
+        return super().fit_generator(model=model,
+                                     dataset=dataset,
+                                     epochs=epochs)
+
+    def train(self):
+        return super().train()
+
+
+if __name__ == '__main__':
+
+    parser = MultiscaleWithConfidenceTrainer.get_parser()
+    args = parser.parse_args()
+
+    trainer = MultiscaleWithConfidenceTrainer(**vars(args))
+    trainer.train()
