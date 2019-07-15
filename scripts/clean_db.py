99870cc81d2d13c263bed2e60b92704d19a96ff9
d.zhukov <d.zhukov@samsung.com>
2019-07-15 14:27:43 +0300

    Tested clean_db script

--- scripts/clean_db.py
+++ scripts/clean_db.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
         exp_id = exp.experiment_id
         for info in client.list_run_infos(exp_id):
             run = client.get_run(info.run_id)
-            if not run.data.metric['successfully_finished']:
-                print(f'Run from experiment {exp.name} run {run.data.params["run_name"]} has been deleted')
+            if not run.data.metrics.get('successfully_finished', False):
+                print(f'Run {run.data.params["run_name"]} from experiment {exp.name} has been deleted')
                 # client.delete_run(run_id)
