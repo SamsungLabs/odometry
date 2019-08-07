ce8e7161096bcd63f80b5497534706375d3c0ae5
d-zhukov <d.zhukov@samsung.com>
2019-08-07 14:36:47 +0300

    VTSU-570 Added flag for keyframe seletor period

--- scripts/slam/dummy_slam_runner.py
+++ scripts/slam/dummy_slam_runner.py
@@ -7,15 +7,23 @@ from slam.models import DummySlam
 
 class DummySlamRunner(BaseSlamRunner):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(knn=20, *args, **kwargs)
+    def __init__(self, knn=20, kfs_period=10, *args, **kwargs):
+        super().__init__(knn=knn, *args, **kwargs)
+        self.kfs_period = kfs_period
 
     def get_slam(self):
         return DummySlam(reloc_weights_path=self.reloc_weights,
                          optflow_weights_path=self.optflow_weights,
                          odometry_model_path=self.odometry_model,
                          knn=self.knn,
-                         input_shapes=self.config['target_size'])
+                         input_shapes=self.config['target_size'],
+                         kfs_period=self.kfs_period)
+
+    @staticmethod
+    def get_parser():
+        parser = super().get_parser()
+        parser.add_argument('--kfs_period', type=int, default=10, help='Period of keyframe selection')
+        return parser
 
 
 if __name__ == '__main__':
