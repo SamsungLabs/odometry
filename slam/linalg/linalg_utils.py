b0a5b8c630044a065e9f21a1d6714908b5229ad6
Филипп Конохов/Visual Understanding Lab /SRR/Assistant Engineer/삼성전자 <f.konokhov@partner.samsung.com>
2019-08-08 14:37:43 +0300

    Update linalg_utils.py

--- slam/linalg/linalg_utils.py
+++ slam/linalg/linalg_utils.py
@@ -32,12 +32,12 @@ def convert_rotation_matrix_to_euler_angles(R):
 def convert_euler_angles_to_rotation_matrix(theta) :
     
     R_x = np.array([[1,         0,                  0                   ],
-                    [0,         math.cos(theta[1]), -math.sin(theta[1]) ],
-                    [0,         math.sin(theta[1]), math.cos(theta[1])  ]
+                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
+                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
                     ])
-    R_y = np.array([[math.cos(theta[0]),    0,      math.sin(theta[0])  ],
+    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
                     [0,                     1,      0                   ],
-                    [-math.sin(theta[0]),   0,      math.cos(theta[0])  ]
+                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
                     ])
     R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
                     [math.sin(theta[2]),    math.cos(theta[2]),     0],
