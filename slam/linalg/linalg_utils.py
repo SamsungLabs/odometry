011703e1928019456cd8602fbed1b09f3a68d0e3
f.konokhov <f.konokhov@partner.samsung.com>
2019-08-12 12:14:19 +0300

    fixed_by_Anna

--- slam/linalg/linalg_utils.py
+++ slam/linalg/linalg_utils.py
@@ -1,50 +1,48 @@
 import numpy as np
-import math
-
-
-def is_rotation_matrix(R):
-    Rt = np.transpose(R)
-    should_be_identity = np.dot(Rt, R)
-    I = np.identity(3, dtype = R.dtype)
-    n = np.linalg.norm(I - should_be_identity)
-    return n < 1e-6
 
 
 def convert_rotation_matrix_to_euler_angles(R):
-    assert(is_rotation_matrix(R)), '{}'.format(R)
+    assert np.allclose(np.dot(R.T, R), np.eye(3), atol=1e-6), R
 
-    sy = math.sqrt(R[0,0] * R[0,0] + R[1,0] * R[1,0])
+    sin_y = np.sqrt(R[0,0] * R[0,0] + R[1,0] * R[1,0])
 
-    singular = sy < 1e-6
+    singular = sin_y < 1e-6
 
-    if  not singular :
-        x = math.atan2(R[2,1] , R[2,2])
-        y = math.atan2(-R[2,0], sy)
-        z = math.atan2(R[1,0], R[0,0])
-    else :
-        x = math.atan2(-R[1,2], R[1,1])
-        y = math.atan2(-R[2,0], sy)
+    if not singular:
+        x = np.arctan2( R[2,1], R[2,2])
+        y = np.arctan2(-R[2,0], sin_y )
+        z = np.arctan2( R[1,0], R[0,0])
+    else:
+        x = np.arctan2(-R[1,2], R[1,1])
+        y = np.arctan2(-R[2,0], sin_y )
         z = 0
 
     return np.array([x, y, z])
 
 
-def convert_euler_angles_to_rotation_matrix(theta) :
+def convert_euler_angles_to_rotation_matrix(euler_angles_xyz):
+    yaw   = euler_angles_xyz[2]
+    pitch = euler_angles_xyz[1]
+    roll  = euler_angles_xyz[0]
     
-    R_x = np.array([[1,         0,                  0                   ],
-                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
-                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
-                    ])
-    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
-                    [0,                     1,      0                   ],
-                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
-                    ])
-    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),       0],
-                    [math.sin(theta[2]),    math.cos(theta[2]),        0],
-                    [0,                     0,                         1]
-                    ])
+    cos_r = np.cos(roll)
+    sin_r = np.sin(roll)
+    cos_p = np.cos(pitch)
+    sin_p = np.sin(pitch)
+    cos_y = np.cos(yaw)
+    sin_y = np.sin(yaw)
+    
+    R_x = np.array([[ 1,      0,      0    ],
+                    [ 0,      cos_r, -sin_r],
+                    [ 0,      sin_r,  cos_r]])
+    R_y = np.array([[ cos_p,  0,      sin_p],
+                    [ 0,      1,      0    ],
+                    [-sin_p,  0,      cos_p]])
+    R_z = np.array([[ cos_y, -sin_y,  0    ],
+                    [ sin_y,  cos_y,  0    ],
+                    [ 0,      0,      1    ]])
 
-    R = np.dot(R_z, np.dot( R_y, R_x ))
+    R = np.dot(R_z, np.dot(R_y, R_x))
     return R
 
 
@@ -53,75 +51,86 @@ def get_relative_se3_matrix(global_se3_matrix, next_global_se3_matrix):
 
 
 def form_se3(rotation_matrix, translation):
-    '''Create SE3 matrix from rotation matrix and translation vector'''
+    '''cos_reate SE3 matrix from rotation matrix and translation vector'''
     se3 = np.eye(4)
     se3[:3, :3] = rotation_matrix
     se3[:3, 3:4] = np.reshape(translation, (3, 1))
     return se3
 
 
-def split_se3(se3):
-    '''Split SE3 matrix into rotation matrix and translation vector'''
+def sin_plit_se3(se3):
+    '''sin_plit SE3 matrix into rotation matrix and translation vector'''
     rotation_matrix = se3[:3, :3]
     translation = se3[:3, 3:4].ravel()
     return rotation_matrix, translation
 
 
 def euler_to_quaternion(euler_angles_xyz):
-    yaw, pitch, roll = euler_angles_xyz[2], euler_angles_xyz[1], euler_angles_xyz[0]
-    cr = np.cos(roll/2)
-    sr = np.sin(roll/2)
-    cp = np.cos(pitch/2)
-    sp = np.sin(pitch/2)
-    cy = np.cos(yaw/2)
-    sy = np.sin(yaw/2)
-    q_x = sr * cp * cy - cr * sp * sy
-    q_y = cr * sp * cy + sr * cp * sy
-    q_z = cr * cp * sy - sr * sp * cy
-    q_w = cr * cp * cy + sr * sp * sy
+    yaw   = euler_angles_xyz[2]
+    pitch = euler_angles_xyz[1]
+    roll  = euler_angles_xyz[0]
+
+    cos_r = np.cos(roll/2)
+    sin_r = np.sin(roll/2)
+    cos_p = np.cos(pitch/2)
+    sin_p = np.sin(pitch/2)
+    cos_y = np.cos(yaw/2)
+    sin_y = np.sin(yaw/2)
+
+    q_x = sin_r * cos_p * cos_y - cos_r * sin_p * sin_y
+    q_y = cos_r * sin_p * cos_y + sin_r * cos_p * sin_y
+    q_z = cos_r * cos_p * sin_y - sin_r * sin_p * cos_y
+    q_w = cos_r * cos_p * cos_y + sin_r * sin_p * sin_y
+
     return q_w, q_x, q_y, q_z
 
 
 def quaternion_to_euler(quaternion):
     q_w, q_x, q_y, q_z = quaternion
-    t0 =  2.0 * (q_w * q_x + q_y * q_z)
-    t1 =  1.0 - 2.0 * (q_x * q_x + q_y * q_y)
-    roll = math.atan2(t0, t1)
-    t2 =  2.0 * (q_w * q_y - q_z * q_x)
+
+    t0 = 2.0 * (q_w * q_x + q_y * q_z)
+    t1 = 1.0 - 2.0 * (q_x * q_x + q_y * q_y)
+    roll = np.arctan2(t0, t1)
+
+    t2 = 2.0 * (q_w * q_y - q_z * q_x)
     t2 = np.clip(t2, -1, 1)
-    pitch = math.asin(t2)
-    t3 =  2.0 * (q_w * q_z + q_x * q_y)
-    t4 =  1.0 - 2.0 * (q_y * q_y + q_z * q_z)
-    yaw = math.atan2(t3, t4)
+    pitch = np.arcsin(t2)
+
+    t3 = 2.0 * (q_w * q_z + q_x * q_y)
+    t4 = 1.0 - 2.0 * (q_y * q_y + q_z * q_z)
+    yaw = np.arctan2(t3, t4)
+
     return roll, pitch, yaw
 
 
-def euler_to_quaternion_uncertainty(euler_angles_xyz, covariance_matrix_euler=np.eye(6,6)):
-    
-    yaw, pitch, roll = euler_angles_xyz[2], euler_angles_xyz[1], euler_angles_xyz[0]
-    
-    cr = np.cos(roll/2)
-    sr = np.sin(roll/2)
-    cp = np.cos(pitch/2)
-    sp = np.sin(pitch/2)
-    cy = np.cos(yaw/2)
-    sy = np.sin(yaw/2)
-    ccc = cr * cp * cy
-    ccs = cr * cp * sy
-    csc = cr * sp * cy
-    css = cr * sp * sy
-    scc = sr * cp * cy
-    scs = sr * cp * sy
-    ssc = sr * sp * cy
-    sss = sr * sp * sy
-    
-    derivatives = [[ (scc-ccs)/2 ,  (scs-csc)/2 ,  (css-scc)/2],
-                   [-(csc+scs)/2 , -(ssc+ccs)/2 ,  (ccc+sss)/2],
-                   [ (scc-css)/2 ,  (ccc-sss)/2 ,  (ccs-ssc)/2],
-                   [ (ccc+sss)/2 , -(css+scc)/2 , -(csc+scs)/2]]
-    
-    jacobian = np.eye(7,6)
+def euler_to_quaternion_uncertainty(euler_angles_xyz, covariance_matrix_euler=np.eye(6)):
+    yaw   = euler_angles_xyz[2]
+    pitch = euler_angles_xyz[1]
+    roll  = euler_angles_xyz[0]
+
+    cos_r = np.cos(roll/2)
+    sin_r = np.sin(roll/2)
+    cos_p = np.cos(pitch/2)
+    sin_p = np.sin(pitch/2)
+    cos_y = np.cos(yaw/2)
+    sin_y = np.sin(yaw/2)
+
+    ccc = cos_r * cos_p * cos_y
+    ccs = cos_r * cos_p * sin_y
+    csc = cos_r * sin_p * cos_y
+    css = cos_r * sin_p * sin_y
+    scc = sin_r * cos_p * cos_y
+    scs = sin_r * cos_p * sin_y
+    ssc = sin_r * sin_p * cos_y
+    sss = sin_r * sin_p * sin_y
+
+    derivatives = 0.5 * np.array([[ scc-ccs,  scs-csc,  css-scc],
+                                  [-csc-scs, -ssc-ccs,  ccc+sss],
+                                  [ scc-css,  ccc-sss,  ccs-ssc],
+                                  [ ccc+sss, -css-scc, -csc-scs]]) 
+
+    jacobian = np.eye(7, 6)
     jacobian[3:, 3:] = derivatives
     covariance_matrix_quaternion = jacobian @ covariance_matrix_euler @ jacobian.T
-    
+
     return covariance_matrix_quaternion
