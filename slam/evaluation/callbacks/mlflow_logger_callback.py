fe17f0c8a7cb515e0dad567cd7155db893d83f86
Анна Воронцова/Visual Understanding Lab /SRR/Engineer/삼성전자 <a.vorontsova@samsung.com>
2019-08-22 10:04:14 +0300

    Update mlflow_logger_callback.py

--- slam/evaluation/callbacks/mlflow_logger_callback.py
+++ slam/evaluation/callbacks/mlflow_logger_callback.py
@@ -8,6 +8,7 @@ class MlflowLogger(keras.callbacks.Callback):
     def __init__(self, prefix=None):
         super().__init__()
         self.prefix = prefix
+        self.add_prefix = lambda k: (self.prefix + '_' + k) if self.prefix else k
 
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
@@ -15,7 +16,8 @@ class MlflowLogger(keras.callbacks.Callback):
         if mlflow.active_run():
             train_loss = logs['loss']
             val_loss = logs.get('val_loss', np.inf)
-            mlflow.log_metric(((self.prefix + '_') if self.prefix else '') + 'train_loss', float(train_loss), step=epoch)
-            mlflow.log_metric(((self.prefix + '_') if self.prefix else '') + 'val_loss', float(val_loss), step=epoch)
+
+            mlflow.log_metric(self.add_prefix('train_loss'), float(train_loss), step=epoch)
+            mlflow.log_metric(self.add_prefix('val_loss'), float(val_loss), step=epoch)
 
         return logs
