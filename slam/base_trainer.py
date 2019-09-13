731e6ed99b19a513ef40ffc8dca631e0afd006e9
Igor Slinko <i.slynko@samsung.com>
2019-09-13 14:20:14 +0300

    Moved mlflow.start_run up (bug)

--- slam/base_trainer.py
+++ slam/base_trainer.py
@@ -87,6 +87,7 @@ class BaseTrainer:
         pass
 
     def start_run(self, exp_name, run_name, exp_dir):
+        mlflow.start_run(run_name=run_name)
         mlflow.log_param('run_name', run_name)
         mlflow.log_param('starting_time', datetime.datetime.now().isoformat())
         mlflow.log_param('epochs', self.epochs)
@@ -111,7 +112,6 @@ class BaseTrainer:
 
         mlflow.set_tracking_uri(self.tracking_uri)
         mlflow.set_experiment(exp_name)
-        mlflow.start_run(run_name=run_name)
 
     def get_dataset(self,
                     train_trajectories=None,
