80f170e4166d28d1cb15fd518b8b025036fd56c5
a.vorontsova <a.vorontsova@samsung.com>
2019-09-19 10:17:30 +0300

    Returned log_param previously deleted by occasion

--- slam/base_trainer.py
+++ slam/base_trainer.py
@@ -122,6 +122,12 @@ class BaseTrainer:
         mlflow.set_experiment(leader_board)
         mlflow.start_run(run_name=run_name)
 
+        mlflow.log_param('run_name', run_name)
+        mlflow.log_param('bundle_name', self.bundle_name)
+        mlflow.log_param('starting_time', datetime.datetime.now().isoformat())
+        mlflow.log_param('epochs', self.epochs)
+        mlflow.log_param('seed', self.seed)
+
     def get_dataset(self,
                     train_trajectories=None,
                     val_trajectories=None):
