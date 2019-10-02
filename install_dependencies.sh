66429f2ba25919b80e03399e7b31ec543a7ae012
Dmitry Zhukov/Visual Understanding Lab /SRR/Engineer/삼성전자 <d.zhukov@samsung.com>
2019-10-02 13:01:34 +0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pip install git+https://www.github.com/keras-team/keras-contrib.git
 conda uninstall -y mlflow
 git clone https://github.com/mlflow/mlflow.git submodules/mlflow
 cd submodules/mlflow
