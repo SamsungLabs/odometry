c6410e0cfc736266bbd59020ab9c3ab60ab48e07
Igor Slinko <i.slynko@samsung.com>
2019-10-21 17:02:31 +0300

    Added param parsers to FlexibleWithAugmentationTrainer

--- scripts/odometry/flexible_with_augmentation_trainer.py
+++ scripts/odometry/flexible_with_augmentation_trainer.py
@@ -3,6 +3,8 @@ import os
 import __init_path__
 import env
 
+import mlflow
+
 from slam.base_trainer import BaseTrainer
 from slam.models import construct_flexible_model
 
@@ -12,15 +14,6 @@ from slam.linalg import Intrinsics, create_optical_flow_from_rt
 class FlexibleWithAugmentationTrainer(BaseTrainer):
 
     def set_model_args(self):
-        self.train_generator_args = {
-            'intrinsics': Intrinsics(f_x=0.5792554391619662,
-                                     f_y=1.91185106382978721,
-                                     c_x=0.48927703464947625,
-                                     c_y=0.4925949468085106,
-                                     width=320,
-                                     height=96),
-            'generate_flow_by_rt_proba': 0.5}
-
         self.construct_model_fn = construct_flexible_model
         self.lr = 0.001
         self.loss = 'mae'
@@ -33,11 +26,42 @@ class FlexibleWithAugmentationTrainer(BaseTrainer):
         self.load_mode = ['flow_xy', 'depth']
         self.preprocess_mode = ['flow_xy', 'depth']
 
+    @staticmethod
+    def get_parser():
+        parser = BaseTrainer.get_parser()
+        parser.add_argument('--generate_flow_by_rt_proba', type=float, default=1)
+        parser.add_argument('--gt_from_uniform_percentile', type=int, default=None)
+        return parser
+
+    def log_params(self):
+        super().log_params()
+        mlflow.log_param('generate_flow_by_rt_proba',
+                         self.train_generator_args['generate_flow_by_rt_proba'])
+        mlflow.log_param('gt_from_uniform_percentile',
+                         self.train_generator_args['gt_from_uniform_percentile'])
+
 
 if __name__ == '__main__':
 
     parser = FlexibleWithAugmentationTrainer.get_parser()
     args = parser.parse_args()
+
+    generate_flow_by_rt_proba = args.generate_flow_by_rt_proba
+    gt_from_uniform_percentile = args.gt_from_uniform_percentile
+
+    del args.generate_flow_by_rt_proba
+    del args.gt_from_uniform_percentile
+
+    args.train_generator_args = {
+        'intrinsics': Intrinsics(f_x=0.5792554391619662,
+                                 f_y=1.91185106382978721,
+                                 c_x=0.48927703464947625,
+                                 c_y=0.4925949468085106,
+                                 width=320,
+                                 height=96),
+        'generate_flow_by_rt_proba': generate_flow_by_rt_proba,
+        'gt_from_uniform_percentile': gt_from_uniform_percentile
+    }
 
     trainer = FlexibleWithAugmentationTrainer(**vars(args))
     trainer.train()
