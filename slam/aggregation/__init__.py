21a8982cc5f75aa78e6f281d81a1389f48d7b21e
d-zhukov <d.zhukov@samsung.com>
2019-10-31 15:25:29 +0300

    Init fix

--- slam/aggregation/__init__.py
+++ slam/aggregation/__init__.py
@@ -1,11 +1,9 @@
 from .dummy_averager import DummyAverager
 from .graph_optimizer import GraphOptimizer
-from .g2o_param_search import grid_search
 from .g2o_estimator import G2OEstimator
 
 __all__ = [
     'DummyAverager',
     'GraphOptimizer',
-    'grid_search',
     'G2OEstimator'
 ]
